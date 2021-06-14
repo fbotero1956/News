434c2c797104b5b4fbbfdfa70ffa384f9a0ff9a6
Felipe Botero <felipe.botero.botero@gmail.com>
2021-06-14 10:38:53 -0400

    added css, js and images

---
+++ articles/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleDeleteView,
+    ArticleUpdateView,
+    ArticleCreateView,
+    ArticleAboutView
+) 
+
+urlpatterns = [
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('create/', ArticleCreateView.as_view(), name='article_create'),
+    path('about/', ArticleAboutView.as_view(), name='about'),
+]
